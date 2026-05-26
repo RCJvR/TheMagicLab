461764b14de6c45e4cf210b5886430570d8b64ee
RCJvR <ruhanjvr@gmail.com>
2026-05-26 21:58:34 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // THE MAGIC LAB — sw.js  (updated for Phase 1)
 // ============================================================
 
-const CACHE_NAME = 'magic-lab-v6'; // bumped from v5 → forces cache refresh
+const CACHE_NAME = 'magic-lab-v7'; // bumped from v6 → forces cache refresh
 
 const urlsToCache = [
   '/',
